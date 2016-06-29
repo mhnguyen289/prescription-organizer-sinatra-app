9f1530da0aea963299ca2c5e258d857e6d4f9feb
Minh <mhnguyen289@gmail.com>
2016-06-29 13:14:47 -0700

    added deleted controllers

--- app/controllers/pills_controller.rb
+++ app/controllers/pills_controller.rb
@@ -1,3 +1,75 @@
-class PillsController < Sinatra::Base
-  
+class PillsController < ApplicationController
+
+  get '/pills/new' do
+  	
+    if logged_in?
+    	if session[:count] != nil
+    		@count = session[:count].to_i
+    	else
+    		@count = 1
+    	end
+      erb :'pills/new'
+    else
+    	redirect '/login'
+    end
+  end
+
+  get '/pills/new/:id' do
+  	if logged_in?
+  		if session[:count] != nil
+  			@count = session[:count].to_i
+  		else
+  			@count = 1
+  		end
+  		@error_msg = params[:error]
+  		@prescription = Prescription.find(params[:id])
+  		erb :'pills/new'
+  	else
+  		redirect '/login'
+  	end
+  end
+
+  get '/pills/:id/edit' do
+  	@pill = Pill.find(params[:id])
+  	@error_msg = params[:error]
+  	@prescription_id = params[:prescription_id]
+  	erb :'pills/edit'
+  end
+
+  get "/pills/:id/delete" do
+      Pill.find(params[:id]).destroy
+      redirect "/prescriptions/#{params[:prescription_id]}/edit"
+  end
+
+  get '/pills/:id/unlink' do
+    @prescription = Prescription.find(params[:prescription_id])
+    temp = @prescription.pill_ids
+    temp.delete(params[:id].to_i)
+    @prescription.pill_ids = temp
+    @prescription.save
+    redirect "/prescriptions/#{params[:prescription_id]}/edit"
+ end
+    
+
+   patch "/pills/:id" do
+   		binding.pry
+    if params[:name] == "" || params[:brand] == "" || params[:exp_date] == ""
+      redirect "/pills/#{params[:id]}/edit?error=Field(s) cannot be left blank"
+    end
+    @pill = Pill.find(params[:id])
+    @pill.name = params[:name]
+    @pill.brand = params[:brand]
+    @pill.exp_date = params[:exp_date]
+    @pill.save
+    redirect "/prescriptions/#{params[:prescription_id]}/edit"
+  end
+
+  get '/pills/:id/delete' do
+    if logged_in?
+      Pill.find(params[:id]).destroy
+      redirect "/prescriptions"
+    else
+      redirect "/login"
+    end
+  end
 end
--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,3 +1,135 @@
-class PresriptionsController < ApplicationController 
-	
+require 'pry'
+
+class PrescriptionsController < ApplicationController
+
+  get '/prescriptions' do 
+    
+    if logged_in?
+      @user = User.find(session[:user_id])
+      @prescriptions = @user.prescriptions
+       erb :'prescriptions/index'
+     else
+       redirect '/login'
+     end
+  end
+
+  post '/prescriptions/:id' do
+
+  	error
+    @prescription = Prescription.find(params[:id])
+    counter = 1
+    while (params["name_#{counter.to_s}"])
+      name = params["name_#{counter.to_s}"]
+      brand = params["brand_#{counter.to_s}"]
+      exp_date = params["exp_date_#{counter.to_s}"]
+      # error checking
+      if name == "" || exp_date == "" || brand == ""
+        redirect "/pills/new/#{@prescription.id}?error=Field(s) cannot be left blan
+        k"
+      elsif Pill.find_by(name: name) != nil
+        redirect "/pills/new/#{@prescription.id}?error=Pill #{name} already exists!"
+      end
+       
+      # while the parameter exists, create the new pill with parameters and increment counter
+      if Pill.find_by(name: name)
+        redirect "/pills/new/#{@prescription.id}?error=#{name} already exists"
+      else
+      @pill = Pill.create(name: params["name_#{counter.to_s}"], 
+        brand: params["brand_#{counter.to_s}"],
+        exp_date: params["exp_date_#{counter.to_s}"])
+      @prescription.pill_ids = (@prescription.pill_ids << @pill.id)
+      @prescription.save
+      end
+      counter += 1
+    end
+    redirect "/prescriptions/#{@prescription.id}"
+  end
+    
+  post '/prescriptions' do
+    if logged_in?
+      if Prescription.find_by(name: params[:name])
+        redirect '/prescriptions/new?error=Prescription name already exists'
+      else
+        # create the prescription and link to checked pills
+        @prescription = Prescription.create(name: params[:name], user_id: session[:user_id], active: false)
+        if params["prescription"]
+          @prescription.pill_ids = params["prescription"]["pill_ids"]
+        end
+        @prescription.save
+        # add number of pills and redirect to new pill form
+        if params["pill_count"] != ""
+          session[:count] = params["pill_count"]
+          redirect "/pills/new/#{@prescription.id.to_s}"
+        else
+        redirect "/prescriptions/#{@prescription.id.to_s}"
+        end
+      end
+    end
+  end
+
+  patch '/prescriptions/:id' do
+    if logged_in?
+      # find prescription and link to checked pills
+      @prescription = Prescription.find(params[:id])
+      @prescription.name = params["name"]
+
+
+      # if there were checked boxes..
+      if params["prescription"]
+        @prescription.pill_ids.each do |id|
+          params["prescription"]["pill_ids"] << id
+        end
+        @prescription.pill_ids = params["prescription"]["pill_ids"]
+      end
+      if params["active"]
+        @prescription.become_active
+      end
+      @prescription.save
+      # add number of pills and redirect to new pill form
+      if params["pill_count"] != ""
+        
+        session[:count] = params["pill_count"]
+        redirect "/pills/new/#{@prescription.id.to_s}"
+      else
+        redirect "/prescriptions/#{@prescription.id.to_s}"
+      end
+    end
+  end
+
+  get '/prescriptions/new' do
+    if logged_in?
+      @error_msg = params[:error]
+      erb :'prescriptions/new'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/prescriptions/:id' do
+    if logged_in?
+      @prescription = Prescription.find(params[:id])
+      erb :'prescriptions/show'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/prescriptions/:id/edit' do
+    if logged_in?
+      @prescription = Prescription.find(params[:id])
+      erb :'prescriptions/edit'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/prescriptions/:id/delete' do
+    if logged_in?
+      Prescription.find(params[:id]).destroy
+      redirect "/prescriptions"
+    else
+      redirect "/login"
+    end
+  end
 end
+
